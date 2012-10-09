3a279a36476acc62491b3d55185862134ce89fd6
Steven Buss <steven.buss@gmail.com>
2012-10-09 00:52:18 -0700

    Move string->[]rune into separate func

--- correction.go
+++ correction.go
@@ -2,28 +2,33 @@ package gospell
 
 import "log"
 
-// Go through every rune and do t.Contains(s[0:n]+s[n+1:len(s)])
-// The distance parameter is currently ignored and is implicitly 1
-func (t *Trie) Deletions(s string, distance int) []string {
+// Convert a string into a slice of runes
+func runes(s string) []rune {
 	// Based on UTF-aware string reversal by Russ Cox.
 	// See http://stackoverflow.com/questions/1752414/how-to-reverse-a-string-in-go
 	n := 0
-	deletions := []string{}
-
 	// First build a list of valid runes
 	runes := make([]rune, len(s))
 	for _, r := range s {
 		runes[n] = r
 		n++
 	}
-
 	// Re-assign runes to get the correct length
 	// Removing this causes invalid memory address errors
 	runes = runes[0:n]
+	return runes
+}
 
+// Go through every rune and do t.Contains(s[0:n]+s[n+1:len(s)])
+// The distance parameter is currently ignored and is implicitly 1
+func (t *Trie) Deletions(s string, distance int) []string {
+	deletions := []string{}
+	runes := runes(s)
+	n := len(runes)
 	c := make([]rune, n-1)
 
 	// Then build substrings
+	// TODO: Respect distance arg, and make this less stupid & ugly
 	for i, _ := range runes {
 		c = []rune{}
 		log.Printf("c is %v", c)
